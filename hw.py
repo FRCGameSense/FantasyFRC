1232e633495604106b097b61b51f314bf581b08f
tytremblay <ty.tremblay@gmail.com>
2014-10-02 12:33:26 -0400

    hello world?

---
+++ hw.py
@@ -0,0 +1,24 @@
+__author__ = 'ttremblay'
+
+from google.appengine.ext import webapp
+
+from google.appengine.ext.webapp import util
+
+class HwHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Hi there!')
+
+def main():
+    application = webapp.WSGIApplication([('/', HwHandler)], debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
